9735361887e545a0a15a6132d3cc5bf254027fd9
brince0304 <brince0304@gmail.com>
2023-06-03 21:43:10 +0900

    -FEAT : 리액트 라우터 돔 의존성 추가 및 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Router, Routes} from "react-router";
+import SignUp from "./pages/SignUp";
+import {Container, styled} from "@mui/material";
+
+const StyledContainer = styled(Container)`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 400px;
+`
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <StyledContainer>
+        <Routes>
+      <Route path={"/signup"} Component={SignUp}/>
+        </Routes>
+    </StyledContainer>
   );
 }
 
